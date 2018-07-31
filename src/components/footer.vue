27ac3f2d44b7f0eb5303e5b4459c45f5c69d1e7a
icharle <icharle@live.com>
2018-07-31 10:27:21 +0800

    添加动态计算当前年份

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="footer">星空学生创新中心丨Copyright©2018</div>
+  <div class="footer">星空学生创新中心丨Copyright©{{year}}</div>
 </template>
 
 <script>
   export default {
-    name: 'footer'
+    computed: {
+      // 动态计算当前年份
+      year () {
+        return new Date().getFullYear()
+      }
+    }
   }
 </script>
 
